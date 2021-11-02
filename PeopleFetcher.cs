c413c87d85eabfdb12237b70b8d57a22c721c095
Eric Duwe <ericduwe@gmail.com>
2021-11-02 12:24:42 -0500

    pushing up completed program

---
+++ PeopleFetcher.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace DogWorx.BadgeMaker {
+    class PeopleFetcher
+    {
+                public static List<Employee> GetEmployees()
+                {
+                List<Employee> employees = new List<Employee>();
+                while (true) {
+                    Console.WriteLine("Please enter a name (or leave empty to exit): ");
+                    // Get a name from the console and assign it to a variable
+                    string firstName = Console.ReadLine();
+                    if (firstName == "") 
+                        {
+                            break;
+                        }
+                    Console.Write("Enter Last Name: ");
+                    string lastName = Console.ReadLine();
+                    Console.Write("Enter ID: ");
+                    int id = Int32.Parse(Console.ReadLine());
+                    Console.Write("Enter Photo URL: ");
+                    string photoUrl = Console.ReadLine();
+                    Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                    employees.Add(currentEmployee);
+                }
+                return employees;
+
+                
+                }
+
+                public static List<Employee> GetFromAPI() {
+                    List<Employee> employees = new List<Employee>();
+                    using (WebClient client = new WebClient())
+                    {
+                        string response = client.DownloadString("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
+                        JObject json = JObject.Parse(response);
+
+                        foreach (JToken token in json.SelectToken("results")) {
+                            Employee emp = new Employee
+                            (
+                                token.SelectToken("name.first").ToString(),
+                                token.SelectToken("name.last").ToString(),
+                                Int32.Parse(token.SelectToken("id.value").ToString().Replace("-", "")),
+                                token.SelectToken("picture.large").ToString()
+                            );
+                            employees.Add(emp);
+                        }
+                    }
+
+                    return employees;
+                }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DogWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Would you like to fetch user data from the API? (Y/N)");
+            string reply = Console.ReadLine();
+            if (reply == "Y" || reply == "y")
+            {
+                List<Employee> employees = PeopleFetcher.GetFromAPI();
+                Util.PrintEmployees(employees);
+                Util.MakeCSV(employees);
+                Util.MakeBadges(employees);
+            }
+            else
+            {
+                List<Employee> employees = PeopleFetcher.GetEmployees();
+                Util.PrintEmployees(employees);
+                Util.MakeCSV(employees);
+                Util.MakeBadges(employees);
+            }
+
+
+
+        }
+
+    }
+
+}
